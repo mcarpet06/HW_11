cfd1205a418eb62df5573eab3a2fe22138c2544a
mcarpet06 <mcarpet06@smail.uni-koeln.de>
2023-01-03 12:52:33 +0100

    anything here

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main2 (String[] args) {
         System.out.println ("Code at line 4");
+        System.out.println ("Code at line 5 was added");
 
     }
 }
