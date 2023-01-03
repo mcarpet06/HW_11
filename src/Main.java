c674fcb044e5ff12fae1e0a39c72234e91072f40
mcarpet06 <mcarpet06@smail.uni-koeln.de>
2023-01-03 20:20:15 +0100

    some Message

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main (String[] args) {
-        System.out.println ("Code at line 4");
+        System.out.println ("Code at line 4 - first step");
 
     }
 }
